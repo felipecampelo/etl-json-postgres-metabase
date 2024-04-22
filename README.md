a9b6600d3bfccdeda9dc822efbb3c0ab2daa7533
Felipe Campelo <felipe.campelo@indicium.tech>
2024-04-21 23:49:06 -0300

    Projeto completo do ETL

---
+++ README.md
@@ -0,0 +1,92 @@
+# Projeto para ETL de dados em aquivos JSON em banco de dados Postgres e visualização no Metabase
+
+### Overview
+
+O propósito do projeto é fazer uma inserção dos dados de arquivos JSON em um banco de dados Postgres. Para isso, se faz necessária uma deduplicação dos dados para não haver registros repetidos no banco de dados destino.
+
+### Instalações necessárias
+
+* Docker
+* Python 3
+
+### Passo a passo de execução
+
+##### Passo 1: Preenchimento do .env.example com os valores das variáveis de ambiente
+
+Nesse momento, é necessário alterar os valores de configuração do banco Postgres. 
+
+```sh
+export POSTGRES_USER="postgres"
+export POSTGRES_PASSWORD="password"
+export POSTGRES_HOST="localhost"
+export POSTGRES_DB="postgres"
+export POSTGRES_PORT=5432
+```
+
+Depois de atualizado, execute o arquivo da seguinte forma:
+
+```sh
+source .env.example
+```
+
+##### Passo 2: Criação do ambiente Python e instalação das bibliotecas necessárias
+
+```sh
+# Criação da env
+python3 -m venv venv
+
+# Ativação da env
+source venv/bin/activate
+
+# Instalações das bibliotecas
+pip install -r requirements.txt
+```
+
+##### Passo 3: Setup do banco Postgres com Docker
+
+```sh
+# Pull da imagem postgres do Docker Hub
+docker pull postgres
+
+# Criação do network
+docker network create my-network
+
+# Criação do volume para persistência dos dados
+docker volume create pgdata
+
+# Criação do container com o Postgres
+docker run -d --name postgres --network my-network -p 5432:5432 -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -v pgdata:/var/lib/postgresql/data postgres
+
+# Pull da imagem do Metabase do Docker Hub
+docker pull metabase/metabase:latest
+
+# Criação do container com o Metabase na mesma network que o Postgres
+docker run -d -p 3000:3000 --name metabase  --network my-network metabase/metabase
+```
+
+Nesse momento, faz-se necessário a configuração do Metabase para conexão com o banco postgres. Para o host, basta colocar o nome que demos ao container, ou seja, `postgres`.
+
+##### Passo 4: ETL dos dados
+
+Basta executar o arquivo `data_load.py` que os dados do arquivo JSON na pasta `data` serão enviados ao banco Postgres.
+
+```python
+python3 data_load.py
+```
+
+### Visualização dos dados no Metabase
+
+Com os dados ingeridos no banco e com conexão entre o Metabase e o Postgres realizada, é possível vermos os dados de forma mais analítica. Abaixo temos um exemplo de um dashboard feito no Metabase:
+
+![dashboard](images/metabase_dashboard.png)
+
+### Logs
+
+Os registros serão salvos na pasta logs, com um status de sucesso ou falha. Abaixo está um exemplo do arquivo de logs.
+
+```txt
+[2024-04-21 19:55:26.546884] Quantidade de registros duplicados e removidos do JSON: 1
+[2024-04-21 19:55:26.547298] Erro ao tentar enviar dados para o banco Postgres: invalid literal for int() with base 10: 'None'
+[2024-04-21 19:56:10.974103] Quantidade de registros duplicados e removidos do JSON: 1
+[2024-04-21 19:57:30.646142] Dados enviados para o banco Postgres com sucesso!
+```
---
+++ data_load.py
@@ -0,0 +1,113 @@
+import os
+from datetime import datetime
+import pandas as pd
+from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy.dialects.postgresql import insert
+import json
+
+
+DATA_FILE_PATH = "data/json_for_case.json"
+LOG_FILE_PATH = "logs/data_insertion.log"
+
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_DB = os.environ.get('POSTGRES_DB')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
+
+
+def save_log(log):
+    current_date_hour = datetime.now()
+    with open(LOG_FILE_PATH, 'a') as f:
+        f.write(f'[{current_date_hour}] {log}\n')
+
+
+def data_handling(data):
+    df = pd.json_normalize(data, sep='_')
+
+    num_rows_before = len(df)
+    df = df.drop_duplicates()
+    num_rows_after = len(df)
+
+    if num_rows_after < num_rows_before:
+        dif = num_rows_before - num_rows_after
+        log = f'Quantidade de registros duplicados e removidos do JSON: {dif}'
+        save_log(log)
+
+    # Add more transfomations here...
+
+    return df
+
+
+def data_extraction(file_path):
+    with open(file_path) as file:
+        data = json.load(file)
+        
+    return data
+    
+
+def send_to_postgres(df, table_name):
+    try:
+        # Creating connection with database
+        engine = create_engine(f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}")
+        upsert_data = df.to_dict(orient='records')
+
+        # Create table if not exists
+        with engine.connect() as connection:
+            connection.execute(f'''
+                CREATE TABLE IF NOT EXISTS {table_name} (
+                    id SERIAL PRIMARY KEY,
+                    nome VARCHAR(255),
+                    idade INTEGER,
+                    email VARCHAR(255),
+                    telefone VARCHAR(20),
+                    endereco_logradouro VARCHAR(255),
+                    endereco_numero INTEGER,
+                    endereco_bairro VARCHAR(255),
+                    endereco_cidade VARCHAR(255),
+                    endereco_estado CHAR(2),
+                    endereco_cep VARCHAR(10)
+                )
+            ''')
+
+            existing_constraints = engine.execute(f"""
+                SELECT constraint_name
+                FROM information_schema.table_constraints
+                WHERE table_name='{table_name}' AND constraint_name='pk_{table_name}_id'
+            """).fetchall()
+
+            if not existing_constraints:
+                connection.execute(f'''
+                    ALTER TABLE {table_name}
+                    ADD CONSTRAINT pk_{table_name}_id PRIMARY KEY (id);
+                ''')
+
+            metadata = MetaData(bind=engine)
+            table = Table(table_name, metadata, autoload=True, autoload_with=engine)
+
+            # Sending data to Postgres
+            for data in upsert_data:
+                stmt = insert(table).values(data)
+                stmt = stmt.on_conflict_do_update(
+                    constraint=f"pk_{table_name}_id",
+                    set_=data
+                )
+                connection.execute(stmt)
+                
+        log = f'Dados enviados para o banco Postgres com sucesso!'
+
+    except Exception as e:
+        log = f'Erro ao tentar enviar dados para o banco Postgres: {e}'
+
+    print(log)
+    save_log(log)    
+
+
+def main():
+    data = data_extraction(DATA_FILE_PATH)
+    df_prep = data_handling(data)
+    send_to_postgres(df_prep, table_name="pessoas")
+
+
+if __name__ == "__main__":
+    main()
